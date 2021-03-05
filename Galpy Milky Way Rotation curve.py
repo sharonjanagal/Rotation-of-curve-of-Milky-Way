8eca141fd92e683a75c19565462ece3dbfb40965
sharonjanagal <75177493+sharonjanagal@users.noreply.github.com>
2021-03-05 11:39:28 +0530

    Update Galpy Milky Way Rotation curve.py

--- Galpy Milky Way Rotation curve.py
+++ Galpy Milky Way Rotation curve.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[28]:
+
 
 
 from galpy.potential import PowerSphericalPotentialwCutoff, MiyamotoNagaiPotential,  NFWPotential
 
 
-# In[29]:
+
 
 
 bp= PowerSphericalPotentialwCutoff(alpha=1.8,rc=1.9/8.,normalize=0.05)
@@ -16,13 +16,13 @@ np= NFWPotential(a=16/8.,normalize=.35)
 MWPotential2014= bp+mp+np
 
 
-# In[30]:
+
 
 
 MWPotential2014[2]*= 1.5
 
 
-# In[31]:
+
 
 
 from galpy.potential import KeplerPotential
@@ -30,13 +30,13 @@ from galpy.util import bovy_conversion
 MWPotential2014wBH= MWPotential2014+KeplerPotential(amp=4*10**6./bovy_conversion.mass_in_msol(220.,8.))
 
 
-# In[ ]:
 
 
 
 
 
-# In[49]:
+
+
 
 
 
@@ -44,7 +44,7 @@ from galpy.potential import plotRotcurve
 plotRotcurve(MWPotential2014wBH,Rrange=[0,4],grid=1001,yrange=[0.,1.2])
 
 
-# In[ ]:
+
 
 
 
